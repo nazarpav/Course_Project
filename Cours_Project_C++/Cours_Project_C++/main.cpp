d755ada3d6d080d6234c97e7377c7cdc64bd0685
павлюк назар <hasab.k18@gmail.com>
2019-04-21 21:06:44 +0300

    the work continues

--- Cours_Project_C++/Cours_Project_C++/main.cpp
+++ Cours_Project_C++/Cours_Project_C++/main.cpp
@@ -54,13 +54,17 @@ public:
 	{
 		this->Products_Name = new_Products_Name;
 	}
-	int Get_Quantity_in_stock()
+	unsigned short Get_Quantity_in_stock()
 	{
 		return this->Quantity_in_stock;
 	}
-	int Get_Cost_in_CU()
+	unsigned short Get_Cost_in_CU()
 	{
 		return this->Cost_in_CU;
+	}
+	unsigned short Get_Cost_in_UAN()
+	{
+		return this->Cost_in_UAN;
 	}
 	string Get_Department()
 	{
@@ -79,52 +83,12 @@ public:
 		return this->Products_Name;
 	}
 };
-class W_W_D
+class Menu_database
 {
-public:
-	void Get_Memory(Product *&product, unsigned short &Quantity_Products)
-	{
-		product = new Product[Quantity_Products];
-	}
-	void Enter_Data_Product(Product product[], unsigned short &Quantity_Products)
-	{
-		unsigned short new_Quantity_in_stock;
-		unsigned short new_Cost_in_CU;
-		string new_Manufacturing_plant;
-		string new_Description;
-		string new_Products_Name;
-		string new_Department;
-		for (unsigned short i = 0; i < Quantity_Products; i++)
-		{
-			cout << "Enter name product> \n" << product << "\n";
-			cin >> new_Products_Name;
-			product[i].Set_Products_Name(new_Products_Name);
-
-			cout << "Enter Quantity in stock> ";
-			cin >> new_Quantity_in_stock;
-			product[i].Set_Quantity_in_stock(new_Quantity_in_stock);
-
-			cout << "Enter Cost in CU> ";
-			cin >> new_Cost_in_CU;
-			product[i].Set_Cost_in_CU(new_Cost_in_CU);
-
-			cout << "Enter Manufacturingplant> ";
-			cin >> new_Manufacturing_plant;
-			product[i].Set_Manufacturing_plant(new_Manufacturing_plant);
-
-			cout << "Enter Description product> ";
-			cin >> new_Description;
-			product[i].Set_Description(new_Description);
-
-			cout << "Enter Department> ";
-			cin >> new_Department;
-			product[i].Set_Department(new_Department);
-		}
-	}
-	void Del_Product(Product product[], unsigned short &Quantity_Products)
+	unsigned short menu_db(Product *&product, unsigned short &Quantity_Products)
 	{
 		unsigned short choise = 1;
-		char Control_Symbol{}, Control_Button{};
+		char Control_Symbol{};
 		const unsigned short size = 10;
 		string menu[size]
 		{
@@ -136,19 +100,12 @@ public:
 			"Fish department",
 			"Grocery department",
 			"Department of drinks",
-			"Department of semi - finished products",
-			"fruit and vegetable department"
+			"Semi-finished products",
+			"Fruit and vegetable"
 		};
 		while (Control_Symbol != 13)
 		{
-			Control_Button = _getch();
 			Control_Symbol = _getch();
-			if (Control_Button == 224)
-			{
-				Control_Button = NULL;
-				Control_Symbol = NULL;
-				continue;
-			}
 			if (Control_Symbol != 72 && Control_Symbol != 80)
 			{
 				continue;
@@ -179,25 +136,118 @@ public:
 			}
 
 		}
-		switch (choise)
+		return choise;
+		
+	}
+public:
+	unsigned short Get_menu_db(Product *&product, unsigned short &Quantity_Products)
+	{
+		return menu_db(product, Quantity_Products);
+	}
+};
+class W_W_D
+{
+	Menu_database menu_DB;
+public:
+	void Get_Memory(Product *&product, unsigned short &Quantity_Products)
+	{
+		product = new Product[Quantity_Products];
+	}
+	void Add_item_Data_Product(Product *&product, unsigned short &Quantity_Products)
+	{
+		Quantity_Products++;
+		/*
+		Product * new_product=new Product[Quantity_Products+1];
+		for (unsigned short i = 0; i < Quantity_Products; i++)
+		{
+			new_product[i] = product[i];
+		}
+		*/
+		product = (Product*)realloc(product, Quantity_Products * sizeof(Product));
+		unsigned short new_Quantity_in_stock;
+		const string block_database = "Bread departmen\t\t1\nPastry Department\t2\nDairy department\t3\nMeat section\t\t4\nSausage department\t5\nFish department\t\t6\nGrocery department\t7\nDepartment of drinks\t8\nSemi-finished products\t9\nFruit and vegetable\t10\n";
+		unsigned short new_Cost_in_CU;
+		string new_Manufacturing_plant;
+		string new_Description;
+		string new_Products_Name;
+		unsigned int new_Department;
+			switch (menu_DB.Get_menu_db(product, Quantity_Products))
+			{
+			case 1:
+				product[Quantity_Products - 1].Set_Department("Bread departmen");
+				break;
+			case 2:
+				product[Quantity_Products - 1].Set_Department("Pastry Department");
+				break;
+			case 3:
+				product[Quantity_Products - 1].Set_Department("Dairy department");
+				break;
+			case 4:
+				product[Quantity_Products - 1].Set_Department("Meat section");
+				break;
+			case 5:
+				product[Quantity_Products - 1].Set_Department("Sausage department");
+				break;
+			case 6:
+				product[Quantity_Products - 1].Set_Department("Fish department");
+				break;
+			case 7:
+				product[Quantity_Products - 1].Set_Department("Grocery department");
+				break;
+			case 8:
+				product[Quantity_Products - 1].Set_Department("Department of drinks");
+				break;
+			case 9:
+				product[Quantity_Products - 1].Set_Department("Semi - finished products");
+				break;
+			case 10:
+				product[Quantity_Products - 1].Set_Department("Fruit and vegetable");
+				break;
+			}
+		cout << "Enter name product> \n" << product << "\n";
+		cin >> new_Products_Name;
+		product[Quantity_Products - 1].Set_Products_Name(new_Products_Name);
+
+		cout << "Enter Quantity in stock> ";
+		cin >> new_Quantity_in_stock;
+		product[Quantity_Products - 1].Set_Quantity_in_stock(new_Quantity_in_stock);
+
+		cout << "Enter Cost in CU> ";
+		cin >> new_Cost_in_CU;
+		product[Quantity_Products - 1].Set_Cost_in_CU(new_Cost_in_CU);
+
+		cout << "Enter Manufacturingplant> ";
+		cin >> new_Manufacturing_plant;
+		product[Quantity_Products - 1].Set_Manufacturing_plant(new_Manufacturing_plant);
+
+		cout << "Enter Description product> ";
+		cin >> new_Description;
+		product[Quantity_Products - 1].Set_Description(new_Description);
+	}
+	void Del_Product(Product *&product, unsigned short &Quantity_Products)
+	{
+		string Products_Name;
+		bool tmp = false;
+		unsigned short index = 0;
+		switch (menu_DB.Get_menu_db(product, Quantity_Products))
 		{
 		case 1:
-			
+
 			break;
 		case 2:
-			
+
 			break;
 		case 3:
-			
+
 			break;
 		case 4:
-			
+
 			break;
 		case 5:
-			
+
 			break;
 		case 6:
-			
+
 			break;
 		case 7:
 
@@ -211,48 +261,55 @@ public:
 		case 10:
 
 			break;
-		default:
-			cout << "Eror!!!\n";
-			break;
 		}
+		for (unsigned short i=0; tmp != true;i++)
+		{
+			cout << "Enter name product> \n" << product << "\n";
+			cin >> Products_Name;
+			if (Products_Name == product[i].Get_Products_Name())
+			{
+				tmp = true;
+				index == i;
+			}
+		}
+		if (tmp == true)
+		{
+
+		}
+		//product[Quantity_Products - 1].Set_Products_Name(Products_Name);
 	}
-	void Show_Product(Product product[], unsigned short &Quantity_Products)
+	void Show_Product(Product *&product, unsigned short &Quantity_Products)
 	{
 		for (unsigned short i = 0; i < Quantity_Products; i++)
 		{
 			cout << product << endl;
-			cout << "ALL Products>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\nProduct >" << product[i].Get_Products_Name() << "\nCost in CU > " << product[i].Get_Cost_in_CU() << "\nQantity in stok> " << product[i].Get_Quantity_in_stock() << "\nManufacturing plant> " << product[i].Get_Manufacturing_plant() << "\nDescription>" << product[i].Get_Description() << endl;
+			cout << "ALL Products>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n"<<"Departament => " <<product[i].Get_Department()<<"\nProduct > " << product[i].Get_Products_Name() << "\nCost in CU > " << product[i].Get_Cost_in_CU() <<"\nCost in UAN > "<<product[i].Get_Cost_in_UAN()<< "\nQantity in stok> " << product[i].Get_Quantity_in_stock() << "\nManufacturing plant> " << product[i].Get_Manufacturing_plant() << "\nDescription>" << product[i].Get_Description() << endl;
 			system("pause");
 		}
 	}
 };
+
 class menu
 {
 protected:
 	void Menu_Private(W_W_D &work_with_database, Product *&product, unsigned short &Quantity_Products, bool &var)
 	{
 		unsigned short choise = 1;
-		char Control_Symbol{}, Control_Button{};
+		char Control_Symbol{};
 		const unsigned short size = 6;
 		string menu[size]
 		{
 			"Show ALL Products",
-			"Enter ALL Data base",
+			"Add item Data base",
 			"Set product size",
-			"______________",
-			"______________",
+			"Delete product",
+			"Redact product",
 			"Exit"
 		};
+
 		while (Control_Symbol != 13)
 		{
-			Control_Button = _getch();
 			Control_Symbol = _getch();
-			if (Control_Button==224)
-			{
-				Control_Button = NULL;
-				Control_Symbol = NULL;
-				continue;
-			}
 			if (Control_Symbol != 72 && Control_Symbol != 80)
 			{
 				continue;
@@ -289,7 +346,7 @@ protected:
 			work_with_database.Show_Product(product, Quantity_Products);
 			break;
 		case 2:
-			work_with_database.Enter_Data_Product(product, Quantity_Products);
+			work_with_database.Add_item_Data_Product(product, Quantity_Products);
 			break;
 		case 3:
 			cout << "Enter Quantity Products> ";
@@ -297,6 +354,8 @@ protected:
 			work_with_database.Get_Memory(product, Quantity_Products);
 			break;
 		case 4:
+			system("cls");
+			cout << "[ Bread department ]\nPastry Department\nDairy department\nMeat section\nSausage department\nFish department\nGrocery department\nDepartment of drinks\nDepartment of semi - finished products\nFruit and vegetable department\n";
 			work_with_database.Del_Product(product, Quantity_Products);
 			break;
 		case 5:
@@ -321,14 +380,14 @@ int main()
 {
 	srand(unsigned(time(NULL)));
 	menu Menu;
-	Product *product = nullptr;
+	Product *product = NULL;
 	unsigned short Quantity_Products = 0;
 	W_W_D work_with_database;
 	bool var = true;
 	while (var)
 	{
 		system("cls");
-		cout << "[ Show ALL Products ]\nEnter ALL Data base\nSet product size\n______________\n______________\nExit\n";
+		cout << "[ Show ALL Products ]\nAdd item Data base\nSet product size\nDelete product\nRedact product\nExit\n";
 		Menu.Menu(work_with_database, product, Quantity_Products, var);
 	}
 
